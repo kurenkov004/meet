b06a47eac166b0c654ad0c2d1ff5f9f8beb8ed73
kurenkov004 <mary.kurenkov@gmail.com>
2024-03-19 17:10:41 -0400

    optimized syntax, added number of events test

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,18 +1,22 @@
 // src/__tests__/EventList.test.js
 
 import { render } from '@testing-library/react';
+import { getEvents } from '../api';
 import EventList from '../components/EventList';
 
 describe('<EventList /> component', () => {
+  let EventListComponent;
+  beforeEach(() => {
+    EventListComponent = render(<EventList />);
+  })
+
   test('has an element with "list" role', () => {
-    const EventListComponent = render(<EventList />);
     expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
   });
 
-  test('renders correct number of events', () => {
-    const EventListComponent = render(<EventList events={
-      [{ id: 1 }, { id: 2 }, {id: 3 }, {id: 4 }]
-    } />);
-    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
-  })
+  test('renders correct number of events', async () => {
+    const allEvents = await getEvents(); 
+    EventListComponent.rerender(<EventList events={allEvents} />);
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
+  });
 });
