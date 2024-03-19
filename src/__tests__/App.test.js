86db2c8de8768a306e9e111fa50f2e2b8e364fe2
kurenkov004 <mary.kurenkov@gmail.com>
2024-03-19 17:09:05 -0400

    optimizing test wording

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,13 +4,20 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 describe('<App /> component', () => {
+  let AppDOM;
+  beforeEach(() => {
+    AppDOM = render(<App />).container.firstChild;
+  })
+
   test('renders list of events', () => {
-    const AppDOM = render(<App />).container.firstChild;
     expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
   });
 
   test('render CitySearch', () => {
-    const AppDOM = render().container.firstChild;
     expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
-    });
+  });
+
+  test('renders NumberOfEvents', () => {
+    expect(AppDOM.querySelector('#number-of-events')).toBeInTheDocument();
+  });
 });
