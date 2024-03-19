8f1985adde7190b323b96daf2141d6b4e154c855
kurenkov004 <mary.kurenkov@gmail.com>
2024-03-19 17:11:41 -0400

    added NumberOfEvents component to root file

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
+
 
 const App = () => {
  return (
    <div className="App">
      <CitySearch />
      <EventList />
+     <NumberOfEvents/>
    </div>
  );
 }
