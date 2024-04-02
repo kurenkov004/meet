ee652d7428c233cbda25baae9832bfefe75928c5
kurenkov004 <mary.kurenkov@gmail.com>
2024-04-02 19:12:59 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -7,7 +7,10 @@ export const extractLocations = (events) => {
 };
 
 export const getEvents = async () => {
+  // NProgress.start();
+
   if (window.location.href.startsWith("http://localhost")) {
+    // NProgress.done();
     return mockData;
   }
 
