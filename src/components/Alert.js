48adcd7d2380128327566afd0f2d0231feb3a0d1
kurenkov004 <mary.kurenkov@gmail.com>
2024-04-20 15:48:02 -0400

    warning Alert implementation

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -48,4 +48,12 @@ class ErrorAlert extends Alert {
   }
 }
 
-export { InfoAlert, ErrorAlert };
+class WarningAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'rgb(250, 90, 30)'; //orange
+    this.bgColor = 'rgb(255, 230, 165)'; //pale yellow
+  }
+}
+
+export { InfoAlert, ErrorAlert, WarningAlert };
