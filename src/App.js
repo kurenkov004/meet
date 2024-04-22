c4fbccee33efdf49953d5d7e52abfc423e8f5f6b
kurenkov004 <mary.kurenkov@gmail.com>
2024-04-22 13:29:04 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
     if (navigator.onLine) {
       setWarningAlert("");
     } else {
-      setWarningAlert("You are offline. The displayed list may not be up to date.");
+      setWarningAlert("You are browsing offline. The displayed data may not be up to date.");
     }
     fetchData();
   }, [currentCity, currentNOE]);
